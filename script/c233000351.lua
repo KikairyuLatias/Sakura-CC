813465ca1f1c1e681ea882d3b9665fe7eabf8ea7
Sakura <44108108+KikairyuLatias@users.noreply.github.com>
2022-05-06 20:54:26 -1000

    Card status updates
    All of the cards have gotten their statuses changed from N/A to Custom Cards (0x20, decimal 32). Hopefully this makes them easier to find/differentiate from the existing stuff.
    
    May get mixed up with your own customs, but as a reminder, my stuff is 233xxxxxx for Master Duel and 221xxxxxxx for Rush Duel. This shouldn't lead to any conflicts; read: shouldn't.

--- script/c233000351.lua
+++ script/c233000351.lua
@@ -16,6 +16,7 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 	--search
 	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
 	e2:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_SINGLE)
